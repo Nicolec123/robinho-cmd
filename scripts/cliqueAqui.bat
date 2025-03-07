bfb91f7a6488388e345ccb9a296e4efd4007ac1b
Nicolec123 <danielefloripa42@gmail.com>
2025-03-07 15:35:50 -0300

    Update cliqueAqui.bat

--- scripts/cliqueAqui.bat
+++ scripts/cliqueAqui.bat
@@ -1,5 +1,12 @@
 @echo off
 echo Iniciando o Robo...
-cd /d "%~dp0"
-start "" "dist\main.exe"
+cd /d "%~dp0"  
+
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Python não encontrado! Verifique se o Python está instalado e no PATH.
+    exit /b
+)
+
+python "%~dp0main.py"  # Força o caminho absoluto para o arquivo main.py
 exit
